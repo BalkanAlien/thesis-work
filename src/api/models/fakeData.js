987ac43dae76eb4009a0cba7e62f62396b053079
Marija Turmakoska <mturmako@ip-192-168-128-225.ec2.internal>
2022-06-20 11:36:04 +0200

    sending some data

---
+++ src/api/models/fakeData.js
@@ -0,0 +1,19 @@
+const fakeData = [
+    {
+        username: 'johncena',
+        cannedmessage: 'hello',
+        location: 'US'
+    },
+    {
+        username: 'amberheard',
+        cannedmessage: 'my dog stepped on a bee',
+        location: 'Neverland'
+    },
+    {
+        username: 'marija',
+        cannedmessage: 'just 1 more semester',
+        location: 'Budapest'
+    }
+];
+
+module.exports = fakeData;
