4ac19082152ed9a24918c62116f2cc36a49e3710
Marija Turmakoska <mturmako@ip-192-168-130-169.ec2.internal>
2022-06-30 14:11:05 +0200

    input validation and error handling of export service

--- modules/export/export.controller.js
+++ modules/export/export.controller.js
@@ -1,9 +1,23 @@
 import { findAll } from "./export.service.js";
 
-export const getAllCannedMessages = async (req, res) => {
+export const getAllCannedMessages = async (req, res, next) => {
+
     const accountId = req.query.accountId;
     const apiKey = req.query.apiKey;
-    const response = await findAll(accountId, apiKey);
-    console.log('messages', response.data);
-    res.send(response.data);
+
+    if (!accountId || !apiKey) {
+        res.status(400).send("Missing an accountId OR an API key.");
+    }
+    //check if accountId does NOT contain only numbers
+    else if (!(/^\d+$/.test(accountId))) {
+        res.status(400).send("The accountId should contain only numbers!");
+    }
+    try {
+        const response = await findAll(accountId, apiKey);
+        console.log('messages', response.data);
+        res.send(response.data);
+    }
+    catch (error) {
+        res.status(500).send("Error found!");
+    }
 };
