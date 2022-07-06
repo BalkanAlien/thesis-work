e329574fffbe904662b39c0eb9bd4fad30efceb4
Marija Turmakoska <mturmako@REM-MTURMAKO-M.home>
2022-07-06 14:29:57 +0200

    clean up

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import 'dotenv/config';
 import { getAllCannedMessages } from "./modules/export/export.controller.js";
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,3 +8,5 @@ app.use(express.json());
 app.route("/export").get(getAllCannedMessages);
 
 app.listen(port);
+
+export default app;
--- test/export.test.js
+++ test/export.test.js
@@ -1,55 +1,45 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import app  from '../index.js';
 
 chai.use(chaiHttp);
-
 const expect = chai.expect;
-//const assert = chai.assert;
 
-//import { getAllCannedMessages } from '../modules/export/export.controller';
-/*
-import { findAll } from '../modules/export/export.service';
-
-import { faker } from '@faker-js/faker';
-*/
 describe("export controller test", () => {
 
-	it('should return 400 if accountId or apiKey are not specified', () => {
-		chai.request('http://localhost:3000')
-		.get('/export')
-		.end((err, res) => {
-			expect(res).to.have.status(400);
-		});
+	it('should return 400 if accountId and apiKey are not specified', () => {
+		chai.request(app)
+			.get('/export')
+			.end((err, res) => {
+				console.log("first test ", res.status);
+				expect(res).to.have.status(400);
+			});
 	});
 
-	it('should return 400 if accountId or apiKey are not specified', () => {
-		chai.request('http://localhost:3000')
-		.get('/export').query({accountId: '232434231'})
-		.end((err, res) => {
-			expect(res).to.have.status(400);
-		});
+	it('should return 400 if apiKey is not specified', () => {
+		chai.request(app)
+			.get('/export').query({ accountId: '232434231' })
+			.end((err, res) => {
+				console.log("2 test ", res.status);
+				expect(res).to.have.status(400);
+			});
 	});
 
-	it('should return 400 if accountId or apiKey are not specified', () => {
-		chai.request('http://localhost:3000')
-		.get('/export').query({apiKey: '232434231'})
-		.end((err, res) => {
-			expect(res).to.have.status(400);
-		});
+	it('should return 400 if accountId is not specified', () => {
+		chai.request(app)
+			.get('/export').query({ apiKey: '232434231' })
+			.end((err, res) => {
+				console.log("3 test ", res.status);
+				expect(res).to.have.status(400);
+			});
 	});
 
 	it('should return 400 if accountId does not contain only numbers', () => {
-		chai.request('http://localhost:3000')
-		.get('/export').query({accountId: '4m2ndos933'})
-		.end((err, res) => {
-			expect(res).to.have.status(400);
-		});
+		chai.request(app)
+			.get('/export').query({ accountId: '4m2ndos933' })
+			.end((err, res) => {
+				console.log("4 test ", res.status);
+				expect(res).to.have.status(400);
+			});
 	});
 });
-/*
-	before(function(done) {
-		accountId = faker.random.alphaNumeric(); 
-		apiKey = faker.datatype.number();
-	});
-*/
-
