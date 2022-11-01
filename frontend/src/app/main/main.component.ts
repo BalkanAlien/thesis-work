59ffa2ea70ef85648591a4c9a7a9b8f773f6f58d
jqgyis@inf.elte.hu <jqgyis@inf.elte.hu>
2022-11-01 18:44:04 +0100

    updated UI

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -15,22 +15,20 @@ export class MainComponent implements OnInit {
   constructor(private gms: GetMessagesService) { }
 
   ngOnInit(): void {
-    this.showCannedMessages();
+    this.showRootMessages();
   }
 
-  showCannedMessages() {
+  showRootMessages() {
     this.gms.getCannedMessages(2307475884, '2307475884%3A3250042652855494%3AT2vPmE97qolpOd78wC6vJtxMkCCI7Rd%2B%3Aalphado1')
     .subscribe((response: any) => {
       console.log(response);
       this.ArrayOfMessages = response;
-    }); 
-    /*for(let i=0; i<this.ArrayOfMessages.length; i++) {
-      if(this.ArrayOfMessages[i].parentId == null || this.ArrayOfMessages[i].folderId == null) {
+    });
+    for(let i=0; i<this.ArrayOfMessages.length; i++) {
+      if(this.ArrayOfMessages[i].folderId == null || this.ArrayOfMessages[i].parentId == null) {
         this.Roots.push(this.ArrayOfMessages[i]);
       }
-    }
-    console.log(this.Roots);*/
+    } 
   }
-
 
 }
